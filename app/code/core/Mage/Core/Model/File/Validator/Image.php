509d3cb1a483ecb212aa55af2478dfb536665be7
Andreas Mautz <andreas.mautz@webvisum.de>
2018-11-29 09:26:58 +0100

    [RELEASE]  Magento Community 1.9.4.0

---
+++ app/code/core/Mage/Core/Model/File/Validator/Image.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Core
+ * @copyright  Copyright (c) 2006-2018 Magento, Inc. (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Validator for check is uploaded file is image
+ *
+ * @category   Mage
+ * @package    Mage_Core
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Core_Model_File_Validator_Image
+{
+    const NAME = "isImage";
+
+    protected $_allowedImageTypes = array(
+        IMAGETYPE_JPEG,
+        IMAGETYPE_GIF,
+        IMAGETYPE_JPEG2000,
+        IMAGETYPE_PNG,
+        IMAGETYPE_ICO,
+        IMAGETYPE_TIFF_II,
+        IMAGETYPE_TIFF_MM
+    );
+
+    /**
+     * Setter for allowed image types
+     *
+     * @param array $imageFileExtensions
+     * @return $this
+     */
+    public function setAllowedImageTypes(array $imageFileExtensions = array())
+    {
+        $map = array(
+            'tif' => array(IMAGETYPE_TIFF_II, IMAGETYPE_TIFF_MM),
+            'tiff' => array(IMAGETYPE_TIFF_II, IMAGETYPE_TIFF_MM),
+            'jpg' => array(IMAGETYPE_JPEG, IMAGETYPE_JPEG2000),
+            'jpe' => array(IMAGETYPE_JPEG, IMAGETYPE_JPEG2000),
+            'jpeg' => array(IMAGETYPE_JPEG, IMAGETYPE_JPEG2000),
+            'gif' => array(IMAGETYPE_GIF),
+            'png' => array(IMAGETYPE_PNG),
+            'ico' => array(IMAGETYPE_ICO),
+            'apng' => array(IMAGETYPE_PNG)
+        );
+
+        $this->_allowedImageTypes = array();
+
+        foreach ($imageFileExtensions as $extension) {
+            if (isset($map[$extension])) {
+                foreach ($map[$extension] as $imageType) {
+                    $this->_allowedImageTypes[$imageType] = $imageType;
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Validation callback for checking is file is image
+     *
+     * @param  string $filePath Path to temporary uploaded file
+     * @return null
+     * @throws Mage_Core_Exception
+     */
+    public function validate($filePath)
+    {
+        list($imageWidth, $imageHeight, $fileType) = getimagesize($filePath);
+        if ($fileType) {
+            if ($this->isImageType($fileType)) {
+                /** if 'general/reprocess_images/active' false then skip image reprocessing. */
+                if (!Mage::getStoreConfigFlag('general/reprocess_images/active')) {
+                    return null;
+                }
+                //replace tmp image with re-sampled copy to exclude images with malicious data
+                $image = imagecreatefromstring(file_get_contents($filePath));
+                if ($image !== false) {
+                    $img = imagecreatetruecolor($imageWidth, $imageHeight);
+                    imagealphablending($img, false);
+                    imagecopyresampled($img, $image, 0, 0, 0, 0, $imageWidth, $imageHeight, $imageWidth, $imageHeight);
+                    imagesavealpha($img, true);
+
+                    switch ($fileType) {
+                        case IMAGETYPE_GIF:
+                            $transparencyIndex = imagecolortransparent($image);
+                            if ($transparencyIndex >= 0) {
+                                imagecolortransparent($img, $transparencyIndex);
+                                for ($y = 0; $y < $imageHeight; ++$y) {
+                                    for ($x = 0; $x < $imageWidth; ++$x) {
+                                        if (((imagecolorat($img, $x, $y) >> 24) & 0x7F)) {
+                                            imagesetpixel($img, $x, $y, $transparencyIndex);
+                                        }
+                                    }
+                                }
+                            }
+                            if (!imageistruecolor($image)) {
+                                imagetruecolortopalette($img, false, imagecolorstotal($image));
+                            }
+                            imagegif($img, $filePath);
+                            break;
+                        case IMAGETYPE_JPEG:
+                            imagejpeg($img, $filePath, 100);
+                            break;
+                        case IMAGETYPE_PNG:
+                            imagepng($img, $filePath);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    imagedestroy($img);
+                    imagedestroy($image);
+                    return null;
+                } else {
+                    throw Mage::exception('Mage_Core', Mage::helper('core')->__('Invalid image.'));
+                }
+            }
+        }
+        throw Mage::exception('Mage_Core', Mage::helper('core')->__('Invalid MIME type.'));
+    }
+
+    /**
+     * Returns is image by image type
+     * @param int $nImageType
+     * @return bool
+     */
+    protected function isImageType($nImageType)
+    {
+        return in_array($nImageType, $this->_allowedImageTypes);
+    }
+}
---
+++ dev/tests/functional/tests/app/Mage/Paypal/Test/TestStep/LoginToPayPalStep.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Tests
+ * @package     Tests_Functional
+ * @copyright  Copyright (c) 2006-2018 Magento, Inc. (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Mage\Paypal\Test\TestStep;
+
+use Mage\Paypal\Test\Fixture\PaypalCustomer;
+use Mage\Paypal\Test\Page\Paypal;
+use Magento\Mtf\TestStep\TestStepInterface;
+use Magento\Mtf\Client\Browser;
+
+/**
+ * Login to Pay Pal step.
+ */
+class LoginToPayPalStep implements TestStepInterface
+{
+    /**
+     * Pay Pal page.
+     *
+     * @var Paypal
+     */
+    protected $paypalPage;
+
+    /**
+     * Pay Pal customer fixture.
+     *
+     * @var PaypalCustomer
+     */
+    protected $customer;
+
+    /**
+     * Browser.
+     *
+     * @var Browser
+     */
+    protected $browser;
+
+    /**
+     * Loader selector.
+     *
+     * @var string
+     */
+    protected $loader = '#spinner .loader';
+
+    /**
+     * @constructor
+     * @param Browser $browser
+     * @param Paypal $paypalPage
+     * @param PaypalCustomer $paypalCustomer
+     */
+    public function __construct(Browser $browser, Paypal $paypalPage, PaypalCustomer $paypalCustomer)
+    {
+        $this->browser = $browser;
+        $this->paypalPage = $paypalPage;
+        $this->customer = $paypalCustomer;
+    }
+
+    /**
+     * Login to Pay Pal.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $reviewBlockIsPresent = false;
+        $sleepingTime = 0;
+        while (!$reviewBlockIsPresent and $sleepingTime <= 60)
+        {
+            sleep(1);
+            $reviewBlockIsPresent = $this->paypalPage->getReviewBlock()->isVisible()
+            or $this->paypalPage->getOldReviewBlock()->isVisible();
+            $sleepingTime++;
+        }
+        /** Log out from previous session. */
+        $reviewBlock = $this->paypalPage->getReviewBlock()->isVisible()
+            ? $this->paypalPage->getReviewBlock()
+            : $this->paypalPage->getOldReviewBlock();
+        $reviewBlock->logOut();
+
+        $reviewBlock->waitLoader();
+        $payPalLoginBlock = $this->getActualBlock();
+        $payPalLoginBlock->fill($this->customer);
+        $payPalLoginBlock->submit();
+        $payPalLoginBlock->switchOffPayPalFrame();
+        $reviewBlock->waitLoader();
+    }
+
+    /**
+     * Returns actual login block by selector
+     *
+     * @return \Mage\Paypal\Test\Block\Login|\Mage\Paypal\Test\Block\OldLogin|\Mage\Paypal\Test\Page\NewLogin
+     */
+    protected function getActualBlock()
+    {
+        if ($this->paypalPage->getNewLoginBlock()->isVisible()) {
+            $returnBlock = $this->paypalPage->getNewLoginBlock();
+        } elseif ($this->paypalPage->getLoginBlock()->isVisible()) {
+            $returnBlock = $this->paypalPage->getLoginBlock();
+        } else {
+            $returnBlock = $this->paypalPage->getOldLoginBlock();
+        }
+        return $returnBlock;
+    }
+}
